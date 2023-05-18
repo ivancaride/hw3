735cb5002d9b779b3d5fdacb5227bd30ee6abd30
ivan caride <ivan.carideferreyra@kellogg.northwestern.edu>
2023-05-18 02:08:34 +0000

    Cambios index

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,55 @@
+class PlacesController < ApplicationController
+
+  def index
+    # find all Place rows
+    @places = Place.all
+  end
+
+  def show
+    @place = place.find_by({ "id" => params["id"] })
+  end
+
+  def new
+      @place = Place.new
+  end
+
+  def create
+    @place = Place.new
+
+    @place["name"] = params["place"]["name"]
+  #save place row
+    @place.save
+  #redirect user
+    redirect_to "/places"
+  end
+
+#REVISAR
+
+  def edit
+    #find a place
+      @place = Place.find_by({ "id" => params["id"] })
+    #render view with edit place form
+  end
+    
+    def update
+    #find a place
+      @place = Place.find_by({ "id" => params["id"] })
+    #assign user-entered form data to place's columns  
+      @place["name"] = params["place"]["name"]
+      
+    #   # save place row
+      @place.save
+    #   # redirect user
+      redirect_to "/places"
+    end
+    
+    def destroy
+    #   # find a place
+      @place = Place.find_by({ "id" => params["id"] })
+        # destroy place row
+      @place.destroy
+    #   # redirect user
+      redirect_to "/places"
+    end    
+  
+end
